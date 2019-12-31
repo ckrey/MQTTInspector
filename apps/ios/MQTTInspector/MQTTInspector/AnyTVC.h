ed851462c5f63b4da047df81ea7127dd6766f385
Christoph Krey <c@ckrey.de>
2019-12-31 17:00:41 +0100

    [FIX] layout warnings

--- apps/ios/MQTTInspector/MQTTInspector/AnyTVC.h
+++ apps/ios/MQTTInspector/MQTTInspector/AnyTVC.h
@@ -3,7 +3,7 @@
 //  MQTTInspector
 //
 //  Created by Christoph Krey on 25.11.19.
-//  Copyright © 2019 Christoph Krey. All rights reserved.
+//  Copyright © 2019-2020 Christoph Krey. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
